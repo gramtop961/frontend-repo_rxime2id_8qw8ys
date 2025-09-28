97db5799df9abc3d93691bb0e719864cae60d3f8
gramtop961 <gramtop961@gmail.com>
2025-09-28 14:06:06 +0530

    Add .next/server/pages/_error.js from template

---
+++ .next/server/pages/_error.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
+R.c("server/chunks/ssr/[root-of-the-server]__e8e5c470._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__72b261d3._.js")
+R.c("server/chunks/ssr/5c34e_next_8bdaf3f8._.js")
+R.c("server/chunks/ssr/[externals]_next_dist_shared_lib_no-fallback-error_external_59b92b38.js")
+R.c("server/chunks/ssr/5c34e_78f4b22e._.js")
+R.m(13573)
+module.exports=R.m(13573).exports
