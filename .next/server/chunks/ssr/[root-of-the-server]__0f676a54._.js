552215cae79a3a61874665c7792375412e184a48
gramtop961 <gramtop961@gmail.com>
2025-09-28 14:05:16 +0530

    Add .next/server/chunks/ssr/[root-of-the-server]__0f676a54._.js from template

---
+++ .next/server/chunks/ssr/[root-of-the-server]__0f676a54._.js
@@ -0,0 +1,3 @@
+module.exports=[93695,(a,b,c)=>{b.exports=a.x("next/dist/shared/lib/no-fallback-error.external.js",()=>require("next/dist/shared/lib/no-fallback-error.external.js"))},66384,a=>{a.n(a.i(80784))},12667,(a,b,c)=>{},44012,a=>{"use strict";a.s(["default",()=>c]);var b=a.i(78279);function c(){return(0,b.jsx)("div",{children:(0,b.jsx)("h1",{children:"Hi"})})}}];
+
+//# sourceMappingURL=%5Broot-of-the-server%5D__0f676a54._.js.map
