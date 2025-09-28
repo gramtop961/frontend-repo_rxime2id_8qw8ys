b0e049c54d95ebf91c809118ddef06ddb6913d89
gramtop961 <gramtop961@gmail.com>
2025-09-28 14:06:00 +0530

    Add .next/server/pages/_document.js from template

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,5 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
+R.c("server/chunks/ssr/[root-of-the-server]__e8e5c470._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__72b261d3._.js")
+R.m(4197)
+module.exports=R.m(4197).exports
