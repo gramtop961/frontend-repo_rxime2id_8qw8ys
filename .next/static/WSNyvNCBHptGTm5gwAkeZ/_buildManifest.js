196bd7a3a378bb5a2fcfacb335999bb6745737df
gramtop961 <gramtop961@gmail.com>
2025-09-28 14:06:10 +0530

    Add .next/static/WSNyvNCBHptGTm5gwAkeZ/_buildManifest.js from template

---
+++ .next/static/WSNyvNCBHptGTm5gwAkeZ/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/184fbaf6367d130e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
